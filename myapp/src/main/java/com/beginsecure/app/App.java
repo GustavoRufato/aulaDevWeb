7150369a9f17b659025c59839696e8549f584b1b
Gustavo Rufato <gustavo.rufato00@gmail.com>
2024-08-12 00:05:39 -0300

    mudança de arquivo

--- myapp/src/main/java/com/beginsecure/app/App.java
+++ myapp/src/main/java/com/beginsecure/app/App.java
@@ -1,13 +1,39 @@
 package com.beginsecure.app;
 
-/**
- * Hello world!
- *
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+        public static void main(String[] args) {
+            String jdbcUrl = "jdbc:mysql://localhost:3306/UniDevWeb";
+            String username = "gustavo";
+            String password = "D3ad.spades";
+    
+            System.out.println("Executando MySQLExample...");
+            System.out.println(username);
+            System.out.println(password);
+    
+            try {
+                Connection connection = DriverManager.getConnection(jdbcUrl, username, password);
+    
+                Statement statement = connection.createStatement();
+    
+                String query = "SELECT * FROM preguica";
+                ResultSet resultSet = statement.executeQuery(query);
+
+                while (resultSet.next()) {
+                    int id = resultSet.getInt("id");
+                    String name = resultSet.getString("nome");
+                    System.out.println("ID: " + id + ", Nome: " + name);
+                }
+    
+                connection.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
-}
+    
